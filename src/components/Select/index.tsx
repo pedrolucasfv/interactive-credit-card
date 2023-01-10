f1a47949ef1829de8e9ca3881f9cb81710031c6c
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-01-10 02:20:07 -0300

    Estilizando o componente Select

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -3,21 +3,23 @@ import { useState } from 'react'
 export type SelectProps = {
   title: string
   items: string[]
+  value?: string
 }
-const Select = ({ items, title }: SelectProps) => {
+const Select = ({ items, title, value }: SelectProps) => {
   const [isOpen, setIsOpen] = useState(false)
   return (
     <S.Wrapper>
-      <S.SelectBox onClick={() => setIsOpen(!isOpen)}>
-        <S.Title role="heading">{title}</S.Title>
-      </S.SelectBox>
       {isOpen && (
         <S.Items aria-hidden={!isOpen} onClick={() => setIsOpen(false)}>
+          <S.Title role="heading">{title}</S.Title>
           {items.map((item, index) => (
-            <S.Title key={index}> {item} </S.Title>
+            <S.Option key={index}> {item} </S.Option>
           ))}
         </S.Items>
       )}
+      <S.SelectBox onClick={() => setIsOpen(!isOpen)}>
+        {value ? <S.Value>{value}</S.Value> : <S.Value>{title}</S.Value>}
+      </S.SelectBox>
     </S.Wrapper>
   )
 }
--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -3,31 +3,44 @@ import styled from 'styled-components'
 export const Wrapper = styled.main`
   font-size: 2rem;
   font-weight: 600;
-  cursor: pointer;
 `
 
-export const Title = styled.div``
+export const Title = styled.div`
+  color: gray;
+  cursor: default;
+`
+export const Value = styled.div``
 
 export const SelectBox = styled.div`
-  height: 5rem;
   padding: 1.5rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
   border-radius: 0.4rem;
+  border: 0.2rem solid gray;
+  cursor: pointer;
 `
 export const Items = styled.div`
-  margin-top: 0.5rem;
   padding: 1.5rem;
-
+  padding-top: 0;
+  margin-bottom: 0.5rem;
   animation: clickAnimation 0.3s forwards;
+  border: 0.2rem solid gray;
 
   @keyframes clickAnimation {
     from {
       height: 0;
     }
     to {
-      height: 17rem;
+      height: 100%;
     }
+  }
+`
+
+export const Option = styled.div`
+  :hover {
+    color: white;
+    background-color: gray;
+    cursor: pointer;
   }
 `
