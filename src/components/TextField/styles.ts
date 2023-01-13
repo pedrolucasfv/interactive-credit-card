6ef81f9c6ee1978d87e0f18393da2c1d7fbb138d
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-01-12 23:46:35 -0300

    input

---
+++ src/components/TextField/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const InputWrapper = styled.div`
+  display: flex;
+`
+export const Wrapper = styled.main``
+export const Label = styled.label``
+export const Input = styled.input``
