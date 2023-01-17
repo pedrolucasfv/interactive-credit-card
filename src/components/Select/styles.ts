1d3a439d7beef509dc246482747a7096aa8abdf5
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-01-16 21:58:50 -0300

    consertando bug do select

--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -4,8 +4,8 @@ export const Wrapper = styled.main`
   font-size: 2rem;
   font-weight: 600;
   background-color: white;
-  width: 100%;
   margin-right: 2rem;
+  position: relative;
 `
 
 export const Title = styled.div`
@@ -16,26 +16,25 @@ export const Value = styled.div``
 
 export const SelectBox = styled.div`
   padding: 1.5rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
   border-radius: 0.4rem;
   border: 0.2rem solid gray;
   cursor: pointer;
 `
 export const Items = styled.div`
+  background-color: white;
   padding: 1.5rem;
   padding-top: 0;
   margin-top: 0.5rem;
   animation: clickAnimation 0.3s forwards;
   border: 0.2rem solid gray;
+  position: absolute;
 
   @keyframes clickAnimation {
     from {
       height: 0;
     }
     to {
-      height: 100%;
+      height: max-content;
     }
   }
 `
